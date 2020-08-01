02f347a2f74c4c6a287e5a30a46fecd8e9bca6d0
SE2019Group1 <r07922037@ntu.edu.tw>
2020-08-01 19:44:38 +0800

    First commit, Docker Compose

---
+++ environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  APIHost: 'http://@SERVER_HOST_VAL@:@SERVER_PORT_VAL@',
+  WSHost: 'ws://@SERVER_HOST_VAL@:@SERVER_PORT_VAL@',
+  SQHost: 'http://@SONAR_HOST_VAL@:@SONAR_PORT_VAL@'
+};
